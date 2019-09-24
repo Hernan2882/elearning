8ff6fa55e7530897037a1bd1f0f0f56ab7f6f166
BANGHO-NB <hernanrios2882@egmail.com>
2019-09-24 01:51:12 -0300

    unidad7

---
+++ codigojavascript.js
@@ -0,0 +1,13 @@
+var nombre = prompt('ingrese nombre de usuario')
+			
+    alert('bienvenido '+nombre+' a nuestro sitio')
+var correo= prompt('ingresa tu correo elctronico')
+var dispositivos_cocina= prompt('ingrese numero de dispositivos en la cocina:' )
+var dispositivos_comedor= prompt('ingrese numero de dispositivos en el comedor:') 
+var lenguaje = prompt('que lenguaje de programacion quieres aprender?')
+var dispositivos_totales = parseInt(dispositivos_comedor)+ parseInt(dispositivos_cocina)
+
+
+
+
+
